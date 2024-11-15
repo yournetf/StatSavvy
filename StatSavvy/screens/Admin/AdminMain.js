b99c7b037e56db895f0ce6f8c2e4ffee261c1c2c
Frank Yournet <yournetf@bxscience.edu>
2024-11-14 21:26:24 -0800

    Implemented admin page base functions.

--- StatSavvy/screens/Admin/AdminMain.js
+++ StatSavvy/screens/Admin/AdminMain.js
@@ -3,30 +3,82 @@ import { useContext } from "react";
 import { UserContext } from "../../App";
 import { DBContext } from "../../App";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import {SPORTSRADAR_API_KEY } from '@env';
+import { collection, getDocs } from "firebase/firestore";
 
 export default function AdminMain(){
 
+    //Imports user information and firestore database from the app 
     const user = useContext(UserContext);
     const db = useContext(DBContext);
 
+    //Gets color theme from the user context.
     const color1 = user[1].theme[0];
     const color2 = user[1].theme[1];
     const color3 = user[1].theme[2];
     const color4 = user[1].theme[3];
 
-    
+    //Options for the API call.
+    const options = {method: "GET", headers: {accept: "application/json"}};
 
+    //method 1.
     const helloWorld = () =>{
         console.log("hello world");
     }
 
+    //method 2.
     const goodbyeWorld = () =>{
         console.log("goodbye world");
     }
 
+    //Function that is formmtted to load API data into player db.
+    const loadCardinalsPlayers = async () =>{
+        try {
+            const response = await fetch(
+                `https://api.sportradar.com/nfl/official/trial/v7/en/seasons/2024/REG/teams/de760528-1dc0-416a-a978-b510d20692ff/statistics.json?api_key=
+              ${SPORTSRADAR_API_KEY}`
+                , options);
+            if (!response.ok) {
+              throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const data = await response.json();
+            const players = data.players;
+            const playersAry = [];
+            players.forEach((player)=>{
+              playersAry.push(player.name);
+            });
+            console.log(playersAry);
+            // await db.collection("LeagueInfo").doc("Arizona Cardinals").update({
+            //   roster: playersAry,
+            // });
+          } catch (err) {
+            console.error("Fetch error:", err);
+          }
+    }
+
+    // Function to print all documents in a collection
+    const printAllDocuments = async () => {
+        try {
+        // Get a reference to the collection
+        const querySnapshot = await getDocs(collection(db, "players"));
+        
+        // Loop through each document in the collection and print it
+        querySnapshot.forEach(doc => {
+            console.log(`Document ID: ${doc.id}\n, Data:`, doc.data());
+            console.log(`--------------------------------------------------`)
+        });
+        } catch (error) {
+        console.error("Error fetching documents: ", error);
+        }
+    }
+    
+
     const functions = [
-        { title: "hey ", function: helloWorld },
+        { title: "hey", function: helloWorld },
         { title: "goodbye", function: goodbyeWorld },
+        { title: "make a call to xyz", function: helloWorld },
+        { title: "load Cardinals players", function: loadCardinalsPlayers},
+        { title: "Print all players", function: printAllDocuments},
     ]
 
     return(
